496225540a81c178bdb90b4d98b43306a6702eab
Rohullah Karimi <rohullahahmad99@gmail.com>
2022-08-22 16:00:12 +0300

    client first edit

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "http://tester.laatulakki.fi:5000/api/";
 //const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 const TOKEN = () => {
     if (
