8bd7a66c635f6ca1aea0291b58ea5ff1d0593202
Rohullah Karimi <rohullahahmad99@gmail.com>
2022-08-23 19:30:01 +0300

    bash 2

--- client/deploy.sh
+++ client/deploy.sh
@@ -2,10 +2,10 @@ echo "Switching to branch master"
 git checkout master
 
 echo "Building app..."
-sudo npm run Build
+npm run Build
 
 echo "Deploying files to server"
-sudo scp -r build/* ec2-user@172.31.87.47:/var/www/laatulakki/client/
+scp -r build/* ec2-user@172.31.87.47:/var/www/laatulakki/client/
 
 
-echo "Done!"
+echo "Done"
