c0c63cf0d5ee44c1723d414e4a0e02128c832685
Rohullah Karimi <rohullahahmad99@gmail.com>
2022-08-23 14:28:21 +0300

    some edits

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -24,8 +24,8 @@ const Products = ({cat, filters, sort}) => {
       try{
         const res = await axiosInstance.get(
           cat 
-            ? `http://localhost:5000/api/products?category=${cat}` 
-            : "http://localhost:5000/api/products"
+            ? `http://tester.laatulakki.fi/api/products?category=${cat}` 
+            : "http://tester.laatulakki.fi/api/products"
         );
         setProducts(res.data);
       }catch(err){
