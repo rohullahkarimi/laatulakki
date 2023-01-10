680cc5573b87c1a123aaa1bd706bde8e0c5c59b4
Rohullah Karimi <rohullahahmad99@gmail.com>
2023-01-10 19:48:04 +0200

    prod

--- client/deploy_prod.sh
+++ client/deploy_prod.sh
@@ -1,11 +1,11 @@
 echo "Switching to branch master"
 git checkout master
 
-echo "Building app..."
+echo "Building prod app..."
 sudo npm run build
 
 echo "Deploying files to server"
-sudo scp -r build/* ec2-user@23.23.248.65:/var/www/laatulakki/client
+sudo scp -r build/* ec2-user@23.23.248.65:/var/www/laatulakki/
 
 
 echo "Done"
