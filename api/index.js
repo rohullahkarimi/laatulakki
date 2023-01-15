161afde24f926651b0bff6452e4804c50c337b23
Rohullah Karimi <rohullahahmad99@gmail.com>
2023-01-15 07:11:31 +0200

    error

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ console.log(`NODE_ENV=${config.NODE_ENV}`);
 
 
 mongoose
-.connect(process.env.MONGO_URL)
+.connect(config.MONGO_URL)
 .then(()=>console.log("DB connection is successful"))
 .catch((err)=>{
     console.log(err);
@@ -38,6 +38,6 @@ app.use("/api/orders", orderRoute);
 app.use("/api/checkout", paytrailRoute);
 app.use("/api/sendMail", sendMail);
 
-app.listen((process.env.PORT || 5000), () => {
+app.listen((config.PORT || 5000), () => {
     console.log("backend server is running!");
 });
