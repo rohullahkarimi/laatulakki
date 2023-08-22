ccaaeb12b342864aa0a275f37e5ac1e5945b269b
Rohullah Karimi <rohullahahmad99@gmail.com>
2023-08-22 14:12:37 +0300

    api changes 3

--- api/index.js
+++ api/index.js
@@ -34,6 +34,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/products/csv", productRoute);
+
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 //app.use("/api/checkout", stripeRoute);
