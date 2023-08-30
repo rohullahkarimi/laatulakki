4c0cc7236e44075b45f4ba7b885b6bff8115220e
Rohullah Karimi <rohullahahmad99@gmail.com>
2023-08-30 13:56:12 +0300

    auguest last version 6

--- client/deploy_stag.sh
+++ client/deploy_stag.sh
@@ -1,10 +1,12 @@
 echo "Switching to branch master"
 git checkout master
 
-# Find the path to env-cmd using 'which env-cmd' and replace '/path/to/env-cmd' with the actual path
-ENV_CMD_PATH=$(which env-cmd)
+# Find the path to env-cmd using 'npm bin'
+ENV_CMD_PATH=$(npm bin)/env-cmd
 
 echo "Building app..."
+echo "ENV_CMD_PATH: $ENV_CMD_PATH"  # Add this line for debugging
+
 if sudo $ENV_CMD_PATH -f .stag.env npm run build:webpack-stag; then
   echo "Build successful. Removing existing files in 'client' directory..."
   sudo rm -rf /var/www/laatulakki/stage/client/*
