44d32da0b0b2a1c374f74ece679ed48b9f92614c
Rohullah Karimi <rohullahahmad99@gmail.com>
2024-01-06 17:42:46 +0200

    url 4

--- admin_dashboard/deploy_prod.sh
+++ admin_dashboard/deploy_prod.sh
@@ -1,11 +1,15 @@
 echo "Switching to branch master"
 git checkout master
 
-echo "Building admin prod app..."
-sudo npm run build
+echo "Building app..."
+if sudo npm run build:webpack-prod; then
+  echo "Build successful. Removing existing files in 'client' directory..."
+  sudo rm -rf /var/www/laatulakki/production/admin_dashboard/*
 
-echo "Deploying admin prod files to server"
-sudo scp -r build/* ec2-user@52.51.192.51:/var/www/laatulakki/production/admin_dashboard
+  echo "Deploying files to server"
+  sudo scp -r dist/* ec2-user@52.51.192.51:/var/www/laatulakki/production/admin_dashboard
 
-
-echo "Done"
+  echo "Done"
+else
+  echo "Build failed. Deployment aborted."
+fi
