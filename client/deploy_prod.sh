b4d9b4903b6874fc9251cb5c35e8b11e1aeacef0
Rohullah Karimi <rohullahahmad99@gmail.com>
2024-01-06 18:02:23 +0200

    env files

--- client/deploy_prod.sh
+++ client/deploy_prod.sh
@@ -1,11 +1,16 @@
 echo "Switching to branch master"
 git checkout master
 
-echo "Building prod app..."
-sudo npm run build
+echo "Building app..."
+if sudo npm run build:webpack-prod --clear-cache; then
+  echo "Build successful. Removing existing files in 'client' directory..."
+  sudo rm -rf /var/www/laatulakki/production/client/*
 
-echo "Deploying files to server"
-sudo scp -r build/* ec2-user@52.51.192.51:/var/www/laatulakki/production/client
+  echo "Deploying files to server"
+  sudo scp -r dist/* ec2-user@52.51.192.51:/var/www/laatulakki/production/client
 
+  echo "Done"
+else
+  echo "Build failed. Deployment aborted."
+fi
 
-echo "Done"
