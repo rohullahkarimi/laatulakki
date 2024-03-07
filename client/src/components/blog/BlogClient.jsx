6d461f59a1aaa9ba7bedd4498e84d889a61bdc5e
Rohullah Karimi <rohullahahmad99@gmail.com>
2024-03-07 19:25:53 +0200

    2

--- client/src/components/blog/BlogClient.jsx
+++
@@ -1,9 +0,0 @@
-import { createClient }  from "@sanity/client";
-
-const clientSanity = createClient({
-  projectId: "dl8tt1w0",
-  dataset: "production",
-  useCdn: true,
-});
-
-export default clientSanity;
